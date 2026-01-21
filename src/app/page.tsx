df93583a6d7c83e22f89ce47d8dce8d8737d1a35
Luciolcjdev <luciodv83@gmail.com>
2026-01-20 22:04:38 -0300

    chore: add prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="bg-slate-500 text-center text-3xl font-semibold text-white underline">
+        Hello world!
+      </h1>
     </>
-  );
+  )
 }
