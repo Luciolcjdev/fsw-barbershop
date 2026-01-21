999c56e1a0b24712519b543b1807e467df67f8ab
Luciolcjdev <luciodv83@gmail.com>
2026-01-20 22:17:16 -0300

    chore: add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import { Button } from "./components/ui/button"
+
 export default function Home() {
-  return (
-    <>
-      <h1 className="bg-slate-500 text-center text-3xl font-semibold text-white underline">
-        Hello world!
-      </h1>
-    </>
-  )
+  return <Button>Hello world!</Button>
 }
