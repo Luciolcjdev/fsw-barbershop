55394053adfa0b7b1b7ec16c5a93749d745aa23b
Luciolcjdev <luciodv83@gmail.com>
2026-01-25 02:12:27 -0300

    feat: add bookings items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./components/header"
 import { Button } from "./components/ui/button"
 import { Input } from "./components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./components/ui/card"
+import { Badge } from "./components/ui/badge"
+import { Avatar, AvatarImage } from "./components/ui/avatar"
 
 export default function Home() {
   return (
@@ -27,9 +30,29 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
-        <div>
-          <h2 className="mt-6 text-xl font-bold">Agendamentos</h2>
-        </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Janeiro</p>
+              <p className="text-2xl">25</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
