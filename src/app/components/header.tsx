fd4d5b12501c9684406fc34f0c5ec6fd5dc12aa0
Luciolcjdev <luciodv83@gmail.com>
2026-01-25 01:28:38 -0300

    feat: add Headers component and banner

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,8 @@
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import Link from "next/link"
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet"
 import { Button } from "./ui/button"
-import { CalendarDaysIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
-import { Separator } from "./ui/separator"
+import { MenuIcon } from "lucide-react"
 
 const Header = () => {
   return (
@@ -25,71 +16,10 @@ const Header = () => {
             height={18}
           />
         </Link>
-        <div className="flex items-center gap-3">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon />
-              </Button>
-            </SheetTrigger>
-            <SheetContent className="rounded-l-3xl">
-              <SheetHeader>
-                <SheetTitle className="item-center flex gap-2 text-xl font-semibold">
-                  Menu
-                </SheetTitle>
-              </SheetHeader>
-              <div className="py-5">
-                <div className="flex justify-between">
-                  <div className="flex items-center gap-3">
-                    <Avatar>
-                      <AvatarImage />
-                      <AvatarFallback>LCJ</AvatarFallback>
-                    </Avatar>
-                    <div>
-                      <h3 className="font-semibold">Lucio Costa Junior</h3>
-                      <span className="block text-xs text-muted-foreground">
-                        luciolcj@gmail.com
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="text-sm font-semibold">
-                <Separator className="mb-8" />
-                <div className="flex flex-col gap-6">
-                  <Link href="/" className="flex items-center gap-3">
-                    <HomeIcon width={20} height={20} />
-                    Início
-                  </Link>
-
-                  <Link href="/my-orders" className="flex items-center gap-3">
-                    <CalendarDaysIcon width={20} height={20} />
-                    Agendamentos
-                  </Link>
-                </div>
-
-                <Separator className="my-8" />
-
-                <div className="flex flex-col gap-6">
-                  <Link href="/category/cabelo">
-                    <Image src="./cabelo.svg" alt="Cabelo" />
-                    Cabelo
-                  </Link>
-                  <Link href="/category/barba">Barba</Link>
-                  <Link href="/category/Acabamento">Acabamento</Link>
-                  <Link href="/category/sobrancelha">Sobrancelha</Link>
-                  <Link href="/category/massagem">Massagem</Link>
-                  <Link href="/category/hidratacao">Hidratação</Link>
-                </div>
-                <Separator className="my-8" />
-                <Button variant="outline">
-                  <LogOutIcon />
-                  Sair da conta
-                </Button>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </div>
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+        {/* <SidebarSheet /> */}
       </CardContent>
     </Card>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
+import { SearchIcon } from "lucide-react"
 import Header from "./components/header"
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Header />
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucio!</h2>
+        <p className="text-xs">Domingo, 25 de Janeiro de 2026</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          {/* <Searchbar /> */}
+          <Input type="search" placeholder="Pesquisar" className="border" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende com os melhores com FSW Barbershop"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <div>
+          <h2 className="mt-6 text-xl font-bold">Agendamentos</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
